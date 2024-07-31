2a0c19f225d023d93138484c0fbca8681e0b1a45
Sterre Govaert <sterre.govaert@student.hogent.be>
2024-08-01 00:14:26 +0700

    small cleanup

--- src/main/java/com/springBoot/ewdj_2024_ep3_festivals/DashboardController.java
+++ src/main/java/com/springBoot/ewdj_2024_ep3_festivals/DashboardController.java
@@ -1,15 +1,11 @@
 package com.springBoot.ewdj_2024_ep3_festivals;
 
-import domain.Genre;
-import domain.Region;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import service.DashboardService;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -20,13 +16,9 @@ public class DashboardController {
 
     @GetMapping
     public String showDashboard(Model model) {
-        List<Genre> genres = dashboardService.findAllGenres();
-        List<Region> regions = dashboardService.findAllRegions();
-        Integer ticketCount = dashboardService.findTicketCountForCurrentUser();
-
-        model.addAttribute("genres", genres);
-        model.addAttribute("regions", regions);
-        model.addAttribute("ticketCount", ticketCount);
+        model.addAttribute("genres", dashboardService.findAllGenres());
+        model.addAttribute("regions", dashboardService.findAllRegions());
+        model.addAttribute("ticketCount", dashboardService.findTicketCountForCurrentUser());
 
         return "dashboard";
     }
