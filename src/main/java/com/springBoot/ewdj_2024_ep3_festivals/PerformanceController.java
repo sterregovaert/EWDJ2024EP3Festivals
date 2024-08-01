cf66ad77b67e7a4b7cdb47ed86d77494c1427261
Sterre Govaert <sterre.govaert@student.hogent.be>
2024-08-01 18:27:35 +0700

    better performancecontroller and performanceService

--- src/main/java/com/springBoot/ewdj_2024_ep3_festivals/PerformanceController.java
+++ src/main/java/com/springBoot/ewdj_2024_ep3_festivals/PerformanceController.java
@@ -33,33 +33,24 @@ public class PerformanceController {
     @GetMapping("/add")
     public String showAddPerformanceForm(@RequestParam("festivalId") Long festivalId, Model model) {
         Performance performance = new Performance();
-        try {
-            performanceService.setupAddPerformanceFormModel(festivalId, performance, model);
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-            return "error";
-        }
+        performanceService.setupAddPerformanceFormModel(festivalId, performance, model);
         return "performance-add";
     }
 
     @PostMapping("/add")
     public String addPerformance(@RequestParam("festivalId") Long festivalId, @Valid @ModelAttribute Performance performance, BindingResult result, Model model) {
-        try {
-            performanceService.setupPerformanceForFestival(festivalId, performance);
-            performanceValidation.validate(performance, result);
+        performanceService.setupPerformanceForFestival(festivalId, performance, result, model);
+        // TODO split up the validate in sub validations
+        performanceValidation.validate(performance, result);
 
-            if (result.hasErrors()) {
-                performanceService.setupAddPerformanceFormModel(festivalId, performance, model);
-                return "performance-add";
-            }
-
-            performanceService.savePerformance(performance);
-            Festival festival = performance.getFestival();
-            return "redirect:/festivals?genre=" + festival.getGenre().getName() + "&region=" + festival.getRegion().getName();
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-            return "error";
+        if (result.hasErrors()) {
+            performanceService.setupAddPerformanceFormModel(festivalId, performance, model);
+            return "performance-add";
         }
+
+        performanceService.savePerformance(performance);
+        Festival festival = performance.getFestival();
+        return "redirect:/festivals?genre=" + festival.getGenre().getName() + "&region=" + festival.getRegion().getName();
     }
 
     // ---- ---- ---- ----
@@ -67,27 +58,17 @@ public class PerformanceController {
     // ---- ---- ---- ----
     @GetMapping("/remove")
     public String showRemovePerformanceForm(@RequestParam("festivalId") Long festivalId, Model model) {
-        try {
-            performanceService.setupRemovePerformanceFormModel(festivalId, model);
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-            return "error";
-        }
+        performanceService.setupRemovePerformanceFormModel(festivalId, model);
         return "performance-remove";
     }
 
     @PostMapping("/remove")
     public String removePerformance(@RequestParam("festivalId") Long festivalId, @RequestParam("performanceId") Long performanceId, RedirectAttributes redirectAttributes) {
-        try {
-            performanceService.deletePerformanceById(performanceId);
-            String successMessage = messageSource.getMessage("performanceRemove.successMessage", null, LocaleContextHolder.getLocale());
-            redirectAttributes.addFlashAttribute("message", successMessage);
-            Festival festival = festivalRepository.findById(festivalId).orElse(null);
-            return "redirect:/festivals?genre=" + festival.getGenre().getName() + "&region=" + festival.getRegion().getName();
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("error", e.getMessage());
-            return "redirect:/performance/remove?festivalId=" + festivalId;
-        }
+        performanceService.deletePerformanceById(performanceId);
+        String successMessage = messageSource.getMessage("performanceRemove.successMessage", null, LocaleContextHolder.getLocale());
+        redirectAttributes.addFlashAttribute("message", successMessage);
+        Festival festival = festivalRepository.findById(festivalId).orElse(null);
+        return "redirect:/festivals?genre=" + festival.getGenre().getName() + "&region=" + festival.getRegion().getName();
     }
 
 
--- src/main/java/service/PerformanceService.java
+++ src/main/java/service/PerformanceService.java
@@ -3,13 +3,19 @@ package service;
 import domain.Festival;
 import domain.Performance;
 import domain.SubGenre;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import repository.FestivalRepository;
 import repository.PerformanceRepository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
+@Service
 public class PerformanceService {
 
     @Autowired
@@ -33,13 +39,9 @@ public class PerformanceService {
         performanceRepository.deleteById(performanceId);
     }
 
-    public void setupAddPerformanceFormModel(Long festivalId, Performance performance, Model model) throws Exception {
-        Festival festival = festivalRepository.findById(festivalId).orElse(null);
 
-        if (festival == null) {
-            throw new Exception("Festival not found");
-        }
-
+    public void setupAddPerformanceFormModel(Long festivalId, Performance performance, Model model) {
+        Festival festival = getFestivalOrReject(festivalId).orElseThrow(() -> new IllegalArgumentException("Festival not found"));
         performance.setFestival(festival);
         if (performance.getStartDateTime() == null) {
             performance.setStartDateTime(festival.getStartDateTime());
@@ -54,25 +56,20 @@ public class PerformanceService {
         model.addAttribute("performance", performance);
     }
 
-    public void setupPerformanceForFestival(Long festivalId, Performance performance) throws Exception {
-        Festival festival = festivalRepository.findById(festivalId).orElse(null);
-
-        if (festival == null) {
-            throw new Exception("Festival not found");
-        }
-
+    public void setupPerformanceForFestival(Long festivalId, Performance performance, BindingResult bindingResult, Model model) {
+        Festival festival = getFestivalOrReject(festivalId).orElseThrow(() -> new IllegalArgumentException("Festival not found"));
         performance.setFestival(festival);
     }
 
-    public void setupRemovePerformanceFormModel(Long festivalId, Model model) throws Exception {
-        Festival festival = festivalRepository.findById(festivalId).orElse(null);
-        if (festival == null) {
-            throw new Exception("Festival not found");
-        }
-
+    public void setupRemovePerformanceFormModel(Long festivalId, Model model) {
+        Festival festival = getFestivalOrReject(festivalId).orElseThrow(() -> new IllegalArgumentException("Festival not found"));
         List<Performance> performances = getPerformancesByFestival(festivalId);
         model.addAttribute("performances", performances);
         model.addAttribute("festival", festival);
+    }
+
+    private Optional<Festival> getFestivalOrReject(Long festivalId) {
+        return festivalRepository.findById(festivalId);
     }
 
 
