ebb2730a2c4b07f97081b1a67c0803ff4bf7e679
Sterre Govaert <sterre.govaert@student.hogent.be>
2024-08-03 23:50:47 +0700

    exceptions from global to festivalErrorAdvice

--- src/main/java/com/springBoot/ewdj_2024_ep3_festivals/FestivalErrorAdvice.java
+++ src/main/java/com/springBoot/ewdj_2024_ep3_festivals/FestivalErrorAdvice.java
@@ -1,13 +1,16 @@
 package com.springBoot.ewdj_2024_ep3_festivals;
 
 import exceptions.FestivalNotFoundException;
+import exceptions.GenreNotFoundException;
 import exceptions.NoArtistsException;
 import exceptions.NoFestivalsException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +21,7 @@ class FestivalErrorAdvice {
     @ResponseBody
     @ExceptionHandler(FestivalNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    Map<String, String> festivalNotFoundHandler(FestivalNotFoundException ex) {
+    public Map<String, String> handleFestivalNotFound(FestivalNotFoundException ex) {
         Map<String, String> response = new HashMap<>();
         response.put("message", ex.getMessage());
         return response;
@@ -27,7 +30,7 @@ class FestivalErrorAdvice {
     @ResponseBody
     @ExceptionHandler(NoFestivalsException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    Map<String, String> noFestivalsFoundHandler(NoFestivalsException ex) {
+    public Map<String, String> handleNoFestivalsException(NoFestivalsException ex) {
         Map<String, String> response = new HashMap<>();
         response.put("message", ex.getMessage());
         return response;
@@ -36,7 +39,7 @@ class FestivalErrorAdvice {
     @ResponseBody
     @ExceptionHandler(NoArtistsException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    Map<String, String> noArtistsFoundHandler(NoArtistsException ex) {
+    public Map<String, String> handleNoArtistsException(NoArtistsException ex) {
         Map<String, String> response = new HashMap<>();
         response.put("message", ex.getMessage());
         return response;
@@ -45,9 +48,25 @@ class FestivalErrorAdvice {
     @ResponseBody
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    Map<String, String> illegalArgumentHandler(IllegalArgumentException ex) {
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
         Map<String, String> response = new HashMap<>();
         response.put("message", ex.getMessage());
         return response;
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Invalid parameter: " + ex.getName());
+        return response;
+    }
+
+    @ExceptionHandler(GenreNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleGenreNotFoundException(GenreNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/springBoot/ewdj_2024_ep3_festivals/GlobalExceptionHandler.java
+++ src/main/java/com/springBoot/ewdj_2024_ep3_festivals/GlobalExceptionHandler.java
@@ -1,20 +1,16 @@
 package com.springBoot.ewdj_2024_ep3_festivals;
 
-import exceptions.*;
+import exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -48,49 +44,5 @@ public class GlobalExceptionHandler {
         return "redirect:/error";
     }
 
-    // TODO check if still necessary for non REST
-    @ExceptionHandler(FestivalNotFoundException.class)
-    public String handleFestivalNotFound(FestivalNotFoundException ex, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
-        return "redirect:/error";
-    }
 
-    @ExceptionHandler(GenreNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleGenreNotFoundException(GenreNotFoundException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(NoArtistsException.class)
-    public ResponseEntity<Map<String, String>> handleNoArtistsException(NoArtistsException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(NoFestivalsException.class)
-    public ResponseEntity<Map<String, String>> handleNoFestivalsException(NoFestivalsException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Invalid parameter: " + ex.getName());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-   
 }
